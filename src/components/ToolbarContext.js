11a339029bdfe66d74395b403efe179be4383ad3
Vladyslav <vladkolodka@gmail.com>
2019-10-06 23:35:08 +0300

    Refactoring

--- src/components/ToolbarContext.js
+++ src/components/ToolbarContext.js
@@ -1,6 +1,6 @@
 import React, {useReducer, useRef} from "react";
 
-const defaultValue = [(() => <span style={{color: 'red'}}>3</span>)()];
+const defaultValue = [];
 export const ToolbarContextAccessor = React.createContext(defaultValue);
 
 export const toolbarActions = {
@@ -9,23 +9,23 @@ export const toolbarActions = {
     update: 'update'
 };
 
-function reducer(state, action) {
+function toolbarReducer(state, action) {
     switch (action.type) {
         case toolbarActions.push:
             return [...state, action.payload];
         case toolbarActions.pop:
-            return state.filter((_, i) => i !== state.length - 1);
+            state.pop();
+            return [...state];
         case toolbarActions.update:
             state[action.payload.index] = action.payload.item;
             return [...state];
-
         default:
             return state;
     }
 }
 
 export function ToolbarContext({children}) {
-    const [state, dispatch] = useReducer(reducer, defaultValue);
+    const [state, dispatch] = useReducer(toolbarReducer, defaultValue);
     const counter = useRef(defaultValue.length - 1);
 
     return <ToolbarContextAccessor.Provider value={{state, dispatch, counter}}>{children}</ToolbarContextAccessor.Provider>;
